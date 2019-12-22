9797e325165640af48abae9b2d04bed093e46d25
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-12-22 12:13:01 +0800

    completed summary of JS arrays

--- 2019/12_December/22_Sunday/arrays.js
+++ 2019/12_December/22_Sunday/arrays.js
@@ -1,12 +1,13 @@
 // Arrays are objects
 
-var data = [1,2,3,4,5];
+var data =  [4, 8, 15, 16, 23, 42];
 
-var add = function(a,b){
+
+var add = function (a, b) {
     return a + b;
 }
 
-data.total = function(){
+data.total = function () {
     return this.reduce(add, 0);
 }
 
@@ -14,3 +15,24 @@ data.sub = 10;
 
 console.log(data.total());
 console.log(data.length);
+
+var is_array = function (value) {
+    return Object.prototype.toString.apply(value) === '[object Array]';
+}
+
+
+Array.prototype.reduce = function (f, value) {
+    var i;
+    for (i = 0; i < this.length; i += 1) {
+        value = f(this[i], value);
+    }
+    return value;
+};
+
+
+var sum = data.reduce(add, 0);
+console.log(sum);
+
+
+console.log(typeof data);
+console.log(is_array(data));
